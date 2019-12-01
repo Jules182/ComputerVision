c00c591d5d848049ca9342c6a332f4e42c187be7
Julian Rengstorf <julianrengstorf@gmail.com>
2019-12-01 16:37:55 +0100

    C++ Template

---
+++ SeamCarving/MainWindow.cpp
@@ -0,0 +1,169 @@
+#include "MainWindow.hpp"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent)
+{
+    /* Initialisiere die UI Komponenten */
+    setupUi();
+}
+
+MainWindow::~MainWindow()
+{
+    /* loesche die UI Komponenten */
+    delete centralWidget;    
+    
+    /* schliesse alle offenen Fenster */
+    cv::destroyAllWindows();
+}
+
+/* Methode oeffnet ein Bild und zeigt es in einem separaten Fenster an */
+void MainWindow::on_pbOpenImage_clicked()
+{
+    /* oeffne Bild mit Hilfe eines Dateidialogs */
+    QString imagePath = QFileDialog::getOpenFileName(this, "Open Image...", QString(), QString("Images *.png *.jpg *.tiff *.tif"));
+    
+    /* wenn ein gueltiger Dateipfad angegeben worden ist... */
+    if(!imagePath.isNull() && !imagePath.isEmpty())
+    {
+        /* ...lese das Bild ein */
+        cv::Mat img = ImageReader::readImage(QtOpencvCore::qstr2str(imagePath));
+        
+        /* wenn das Bild erfolgreich eingelesen worden ist... */
+        if(!img.empty())
+        {
+            /* ...merke das Originalbild... */
+            originalImage = img;
+            
+            /* ...aktiviere das UI... */
+            enableGUI();
+            
+            /* ...zeige das Originalbild in einem separaten Fenster an */
+            cv::imshow("Original Image", originalImage); 
+        }
+        else
+        {
+            /* ...sonst deaktiviere das UI */
+            disableGUI();
+        }
+    }
+}
+
+void MainWindow::on_pbComputeSeams_clicked()
+{
+    /* Anzahl der Spalten, die entfernt werden sollen */
+    int colsToRemove = sbCols->value();
+    
+    /* Anzahl der Zeilen, die entfernt werden sollen */
+    int rowsToRemove = sbRows->value();
+    
+    /* .............. */
+}
+
+void MainWindow::on_pbRemoveSeams_clicked()
+{
+    /* .............. */
+}
+
+void MainWindow::setupUi()
+{
+    /* Boilerplate code */
+    /*********************************************************************************************/
+    resize(129, 211);
+    QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    setSizePolicy(sizePolicy);
+    setMinimumSize(QSize(129, 211));
+    setMaximumSize(QSize(129, 211));
+    centralWidget = new QWidget(this);
+    centralWidget->setObjectName(QString("centralWidget"));
+    
+    horizontalLayout = new QHBoxLayout(centralWidget);
+    verticalLayout = new QVBoxLayout();
+    
+    pbOpenImage = new QPushButton(QString("Open Image"), centralWidget);
+    verticalLayout->addWidget(pbOpenImage);
+    
+    
+    verticalLayout_3 = new QVBoxLayout();
+    lCaption = new QLabel(QString("Remove"), centralWidget);
+    lCaption->setEnabled(false);
+    verticalLayout_3->addWidget(lCaption);
+    
+    horizontalLayout_3 = new QHBoxLayout();
+    horizontalLayout_3->setObjectName(QString("horizontalLayout_3"));
+    lCols = new QLabel(QString("Cols"), centralWidget);
+    lCols->setEnabled(false);
+    lRows = new QLabel(QString("Rows"), centralWidget);
+    lRows->setEnabled(false);
+    horizontalLayout_3->addWidget(lCols);
+    horizontalLayout_3->addWidget(lRows);
+    verticalLayout_3->addLayout(horizontalLayout_3);
+    
+    horizontalLayout_2 = new QHBoxLayout();
+    sbCols = new QSpinBox(centralWidget);
+    sbCols->setEnabled(false);
+    horizontalLayout_2->addWidget(sbCols);
+    sbRows = new QSpinBox(centralWidget);
+    sbRows->setEnabled(false);
+    horizontalLayout_2->addWidget(sbRows);
+    verticalLayout_3->addLayout(horizontalLayout_2);
+    verticalLayout->addLayout(verticalLayout_3);
+    
+    pbComputeSeams = new QPushButton(QString("Compute Seams"), centralWidget);
+    pbComputeSeams->setEnabled(false);
+    verticalLayout->addWidget(pbComputeSeams);
+    
+    pbRemoveSeams = new QPushButton(QString("Remove Seams"), centralWidget);
+    pbRemoveSeams->setEnabled(false);
+    verticalLayout->addWidget(pbRemoveSeams);
+    
+    verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    verticalLayout->addItem(verticalSpacer);
+    horizontalLayout->addLayout(verticalLayout);
+    
+    horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    horizontalLayout->addItem(horizontalSpacer);
+    setCentralWidget(centralWidget);
+    /*********************************************************************************************/
+    
+    
+    /* Verbindung zwischen den Buttonklicks und den Methoden, die beim jeweiligen Buttonklick ausgefuehrt werden sollen */
+    connect(pbOpenImage,    &QPushButton::clicked, this, &MainWindow::on_pbOpenImage_clicked);  
+    connect(pbComputeSeams, &QPushButton::clicked, this, &MainWindow::on_pbComputeSeams_clicked); 
+    connect(pbRemoveSeams,  &QPushButton::clicked, this, &MainWindow::on_pbRemoveSeams_clicked);
+}
+
+void MainWindow::enableGUI()
+{
+    lCaption->setEnabled(true);
+    
+    lCols->setEnabled(true);
+    lRows->setEnabled(true);
+    
+    sbCols->setEnabled(true);
+    sbRows->setEnabled(true);
+    
+    pbComputeSeams->setEnabled(true);
+    pbRemoveSeams->setEnabled(true);
+    
+    sbRows->setMinimum(0);
+    sbRows->setMaximum(originalImage.rows);
+    sbRows->setValue(2);
+    
+    sbCols->setMinimum(0);
+    sbCols->setMaximum(originalImage.cols);
+    sbCols->setValue(2);
+}
+
+void MainWindow::disableGUI()
+{
+    lCaption->setEnabled(false);
+    
+    lCols->setEnabled(false);
+    lRows->setEnabled(false);
+    
+    sbCols->setEnabled(false);
+    sbRows->setEnabled(false);
+    
+    pbComputeSeams->setEnabled(false);
+    pbRemoveSeams->setEnabled(false);
+}
